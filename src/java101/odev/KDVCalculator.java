cac94816842577552c26b8f47cb6e21655e4c5a4
qiran <qiran637@gmail.com>
2022-02-06 23:13:07 +0300

    "Java101 odev2: KDV Tutarı Hesaplayan Program" has been completed.

---
+++ src/java101/odev/KDVCalculator.java
@@ -0,0 +1,18 @@
+package java101.odev;
+
+import java.util.Scanner;
+
+public class KDVCalculator {
+    public static void main(String[] args) {
+        double tutar, kdvOran;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ücret Tutarını Giriniz : ");
+        tutar = input.nextDouble();
+        kdvOran = tutar > 1000 ? 0.08 : 0.18;
+        double kdvTutar = tutar * kdvOran;
+        double kdvliTutar = tutar + kdvTutar;
+        System.out.printf("KDV'siz  Fiyat\t:   %.2f TL\n", tutar);
+        System.out.printf("KDV tutari    \t:   %.2f TL\n", kdvTutar);
+        System.out.printf("KDV'li Fiyat\t:   %.2f TL\n", kdvliTutar);
+    }
+}
