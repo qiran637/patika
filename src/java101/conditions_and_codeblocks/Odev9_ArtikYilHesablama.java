1b13900514725570de1a0ef934cc662dbb6c9fe0
qiran <qiran637@gmail.com>
2022-02-08 18:15:45 +0300

    "Java101- ArtikYilHesablamaProgram" has been updated.

--- src/java101/conditions_and_codeblocks/Odev9_ArtikYilHesablama.java
+++ src/java101/conditions_and_codeblocks/Odev9_ArtikYilHesablama.java
@@ -5,21 +5,25 @@ import java.util.Scanner;
 public class Odev9_ArtikYilHesablama {
     public static void main(String[] args) {
         int year;
+        boolean isArti=false;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Yıl Giriniz : ");
         year = input.nextInt();
 
-        if (year%4 == 0)
-            if (year%100==0)
-                if (year%400==0)
-                    System.out.println(year + " bir artık yıldır !");
-                else
-                    System.out.println(year + " bir artık yıl değildir !");
-            else
-                System.out.println(year + " bir artık yıldır !");
+        if (year%4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0)
+                    isArti = true;
+            } else
+                isArti = true;
+        }
+
+        if (isArti)
+            System.out.println(year + " bir artık yıldır !");
         else
             System.out.println(year + " bir artık yıl değildir !");
+
 
     }
 }
