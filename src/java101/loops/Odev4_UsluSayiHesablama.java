a64b14e619046263e14807c55ed0766a6f012af9
qiran <qiran637@gmail.com>
2022-02-10 22:11:06 +0300

    "Java101- Odev4_UsluSayiHesablama" has been completed.

---
+++ src/java101/loops/Odev4_UsluSayiHesablama.java
@@ -0,0 +1,30 @@
+package java101.loops;
+
+import java.util.Scanner;
+
+public class Odev4_UsluSayiHesablama {
+    public static void main(String[] args) {
+        /*
+       *
+       *
+       * Ödev
+       * Java ile kullanıcının girdiği değerler ile üslü sayı hesaplayan
+       * programı "For Döngüsü" kullanarak yapınız.
+       *
+       */
+
+        int n, k, toplam = 1;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Üssü alanacak sayı : ");
+        n = input.nextInt();
+        System.out.println("Üs olacak sayı : ");
+        k = input.nextInt();
+
+        for (int i = 0; i < k; i++) {
+            toplam *= n;
+        }
+
+        System.out.println(n + "^" + k + " = " + toplam);
+
+    }
+}
