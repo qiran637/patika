1325b6e0688fcf3f8a8a086683171103a3a5e2fc
qiran <qiran637@gmail.com>
2022-02-10 22:36:47 +0300

    "Java101- Odev5_BasamakSayisiToplama" has been completed.

---
+++ src/java101/loops/Odev5_BasamakSayisiToplama.java
@@ -0,0 +1,28 @@
+package java101.loops;
+
+import java.util.Scanner;
+
+public class Odev5_BasamakSayisiToplama {
+    public static void main(String[] args) {
+        /*
+         * Ödev
+         * Bir sayının basamak sayılarının toplamını hesaplayan program yazınız.
+         * Örnek : 1643 = 1 + 6 + 4 + 3 = 14
+         */
+        long  number, basNumber;
+        int result=0;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Giriniz : ");
+        number = input.nextLong();
+
+
+        while (number != 0) {
+            basNumber = number%10;
+            result += basNumber;
+            number /=10;
+        }
+
+        System.out.println("Sonuç : " + result);
+
+    }
+}
