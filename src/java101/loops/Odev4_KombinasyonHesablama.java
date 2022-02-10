1b2dddcfbdd26d5cf5d41765e7a188d8b44e8dcb
qiran <qiran637@gmail.com>
2022-02-10 21:58:30 +0300

    "Java101- Odev4_KombinasyonHesablama" has been optimized.

--- src/java101/loops/Odev4_KombinasyonHesablama.java
+++ src/java101/loops/Odev4_KombinasyonHesablama.java
@@ -6,24 +6,26 @@ public class Odev4_KombinasyonHesablama {
     public static void main(String[] args) {
 
         //C(n,r) = n! / (r! * (n-r)!)
-        int n, r, fakn = 1, fakr = 1, fakf=1;
+        int n, r, fakn = 1, fakr = 1, fakf=1, combination;
         Scanner input = new Scanner(System.in);
         System.out.print("n'in degerini giriniz : ");
         n = input.nextInt();
         System.out.print("r'in degerini giriniz : ");
         r = input.nextInt();
 
-        for (int i = 1; i <= n; i++) {
-            fakn *= i;
-            if (i <= r) {
-                fakr *= i;
-            }
-            if (i <= n - r) {
-                fakf *= i;
-            }
-        }
+        if (n>=r){
 
-        System.out.println("C(" + n + "," + r + ") = " + fakn/(fakr*fakf));
+            for (int i = 1; i <= n; i++) {
+                fakn *= i;
+                if (i <= r) fakr *= i;
+                if (i <= n - r) fakf *= i;
+            }
+
+            combination = fakn/(fakr*fakf);
+            System.out.println("C(" + n + "," + r + ") = " + combination);
+
+        } else
+            System.out.println("n>=r olmalidir.");
 
     }
 }
