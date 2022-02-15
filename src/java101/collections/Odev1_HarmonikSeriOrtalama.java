99449183a724696ad4be77e2596e5c11999d77e9
qiran <qiran637@gmail.com>
2022-02-15 13:36:04 +0300

    "Java101 - Diziler - Odev1_HarmonikSeriOrtalama" has been completed.

---
+++ src/java101/collections/Odev1_HarmonikSeriOrtalama.java
@@ -0,0 +1,15 @@
+package java101.collections;
+
+public class Odev1_HarmonikSeriOrtalama {
+
+
+    public static void main(String[] args) {
+        int[] dizi = {15, 26, 35, 27, 45, 56, 49, 65, 39};
+        double harmonikToplam = 0.0;
+
+        for (int i = 1; i <= dizi.length; i++) {
+            harmonikToplam += 1.0 / dizi[i];
+        }
+        System.out.println(dizi.length / harmonikToplam);
+    }
+}
