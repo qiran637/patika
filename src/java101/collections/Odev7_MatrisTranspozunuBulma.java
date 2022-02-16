95e43212ae6fb8bbff6a45b710d1fedda7247945
qiran <qiran637@gmail.com>
2022-02-16 14:58:20 +0300

    "Java101 - Diziler - Odev7_MatrisTranspozunuBulma" has been updated.

--- src/java101/collections/Odev7_MatrisTranspozunuBulma.java
+++ src/java101/collections/Odev7_MatrisTranspozunuBulma.java
@@ -1,13 +1,10 @@
 package java101.collections;
 
-import java.util.Arrays;
-
 public class Odev7_MatrisTranspozunuBulma {
     public static void main(String[] args) {
 
         int[][] matrisa = {{ 2, 3, 4}, {5, 6, 7}};
-        int[] tempArr = new int[matrisa.length * matrisa[0].length];
-        int[][] transpoze = new int[matrisa[0].length][matrisa.length];
+        int[][] matrisa_T = new int[matrisa[0].length][matrisa.length];
 
         // print the Matrix.
         System.out.println("Matris : ");
@@ -20,19 +17,14 @@ public class Odev7_MatrisTranspozunuBulma {
         //Find the Transpose of matrix
         for (int i = 0; i < matrisa.length; i++) {
             for (int j = 0; j < matrisa[i].length; j++) {
-                tempArr[matrisa[i].length * i + j] = matrisa[i][j];
+                matrisa_T[j][i] = matrisa[i][j];
             }
         }
 
-        for (int i = 0; i < transpoze.length; i++) {
-            for (int j = 0; j < transpoze[i].length; j++) {
-                transpoze[i][j] = tempArr[transpoze.length * j + i];
-            }
-        }
 
         //print the Transpose
         System.out.println("Transpoze : ");
-        for (int[] a:transpoze){
+        for (int[] a:matrisa_T){
             for (int i : a)
                 System.out.print(i + "   ");
             System.out.println();
